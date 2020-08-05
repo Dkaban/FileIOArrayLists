0936a27fcffc31b12b57d22c1a0d06c854c5bb58
Dustin <dustinkaban@gmail.com>
2020-08-05 13:51:20 -0700

    Update Clown.java

--- Clown.java
+++ Clown.java
@@ -1,8 +1,7 @@
 //***********************************************************
 //  Clown.java
 //
-//  Dustin Kaban T00663749
-//  COMP 1231 Module 4 Assignment 4 Question 1
+//  Dustin Kaban
 //  July 29th, 2020
 //
 //  This class creates a Clown, only a name is required.
